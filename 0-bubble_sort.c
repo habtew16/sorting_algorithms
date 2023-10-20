2845adc585cc21cf2b487b277eaba22f3c22b30e
Habtamu <habtamuwolde96@gmail.com>
2023-10-20 03:50:38 -0700

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - function to do bubble sort
+ * @array: list of numbers to be sorted
+ * @size: the length of the array
+ * no return
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	int swapped;
+	size_t i;
+	size_t tmp;
+
+	int count = 0;
+
+	do {
+		swapped = 0;
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				swapped = 1;
+				print_array(array, size);
+				count++;
+			}
+		}
+	} while (swapped);
+
+	printf("%d", count);
+}
