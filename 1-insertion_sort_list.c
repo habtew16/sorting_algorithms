f7e3586857bd1c4177cddf0c68e464ef67010464
Habtamu <habtamuwolde96@gmail.com>
2023-10-23 04:19:06 -0700

    insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,50 +1,37 @@
 #include "sort.h"
 
 /**
- * insertion_sort - function that sorts lists with
+ * insertion_sort_list - function that sorts lists with
  * using doubly linked list
  * @list: array of elements to be sorted
- * no returnr
+ * no return
  */
-
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current = (*list)->next;
-	listint_t *insertion;
-	listint_t *temp
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+	listint_t *tmp;
+	int n;
 
-    *current = (*list)->next;
-
-    while (current != NULL)
-    {
-        *insertion = current;
-
-        while (insertion->prev != NULL && insertion->n < insertion->prev->n)
-        {
-           *temp = insertion->prev;
-
-            if (insertion->next != NULL)
-                insertion->next->prev = temp;
-
-            temp->next = insertion->next;
-            insertion->next = temp;
-            insertion->prev = temp->prev;
-
-            if (temp->prev != NULL)
-                temp->prev->next = insertion;
-            else
-                *list = insertion;
-
-            temp->prev = insertion;
-
-            insertion = temp;
-            print_list(*list);
-        }
-
-        current = current->next;
-    }
+	if (!list)
+		return;
+	tmp = *list;
+	while (tmp)
+	{
+		while (tmp)
+		{
+			if (tmp->next)
+			{
+				if (tmp->n > tmp->next->n)
+				{
+					n = tmp->n;
+					*(int *)&tmp->n = tmp->next->n;
+					*(int *)&tmp->next->n = n;
+					tmp = *list;
+					print_list(*list);
+					break;
+				}
+			}
+			tmp = tmp->next;
+		}
+	}
 }
-
